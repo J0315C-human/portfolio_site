9d199ace0ef5d87025b37dbf36f5ed75a092f6b4
J0315C-human <foibles@gmail.com>
2018-12-14 14:52:11 -0500

    save merging/comparing until the end

--- src/editor/serializer.ts
+++ src/editor/serializer.ts
@@ -46,26 +46,13 @@ const compressFramesToDeltas = (frames: Frame[]) => {
   return compressed;
 }
 
-const compressFrames = (frames: Frame[]) => {
-  const compDeltas = compressFramesToDeltas(frames);
-  const compString = compressFramesToStringGrids(frames);
-  const best: FrameCompressed[] = [];
-  for (let i = 0; i < frames.length; i++) {
-    const deltLength = JSON.stringify(compDeltas[i]).length;
-    const strLength = JSON.stringify(compString[i]).length;
-    if (deltLength < strLength) { best.push(compDeltas[i]); }
-    else { best.push(compString[i]); }
-  }
-  return best;
-}
-
 const encodeFramesCompressed = (frames: FrameCompressed[]) => {
   return frames.map(frame => {
     if (frame.t === 'a') { return encodeFrameCompressedWithDeltaList(frame); }
     else if (frame.t === 'b') { return encodeFrameCompressedWithDeltaListSameColor(frame); }
     else if (frame.t === 'c') { return encodeFrameCompressedWithStringGrid(frame); }
     else return 'ERROR';
-  }).join(';');
+  });
 }
 const encodeFrameCompressedWithDeltaList = (frame: FrameCompressedWithDeltaList) => {
   const { f, w, d } = frame;
@@ -81,23 +68,57 @@ const encodeFrameCompressedWithStringGrid = (frame: FrameCompressedWithStringGri
   return `${f},${w}^${g.join(':')}`;
 }
 
-const saveToLocalStorage = (frames: Frame[]) => {
-  const compressed = compressFrames(frames);
-  const encoded = encodeFramesCompressed(compressed);
-  const shortened = addEncodings(encoded);
-  const s1 = JSON.stringify(frames).length;
-  const s2 = JSON.stringify(compressed).length;
-  const s3 = encoded.length;
-  const s4 = shortened.length;
-  console.log(`uncompressed: ${s1} chars / ${(s1 / 1024).toFixed(2)} KB`);
-  console.log(`--compressed: ${s2} chars / ${(s2 / 1024).toFixed(2)} KB, ` + (100 * s2 / s1).toFixed(2) + ' %');
-  console.log(`-----encoded: ${s3} chars / ${(s3 / 1024).toFixed(2)} KB, ` + (100 * s3 / s2).toFixed(2) + ' %');
-  console.log(`---shortened: ${s3} chars / ${(s4 / 1024).toFixed(2)} KB, ` + (100 * s4 / s3).toFixed(2) + ' %');
-  console.log('ratio: ' + (100 * s4 / s1).toFixed(2) + ' %');
-  window.localStorage.setItem('animation', shortened);
+const mergeCompressionResults = (a: string[], b: string[]) => {
+  const best = [];
+  a.forEach((str, i) => {
+    if (str.length <= b[i].length) {
+      best.push(str);
+    } else {
+      best.push(b[i]);
+    }
+  })
+  return best;
 }
 
+const stats = (x: number) => `${x} chars / ${(x / 1024).toFixed(2)} KB`;
 
+const compressAndEncodeFrames = (frames: Frame[]) => {
+  const compDeltas = compressFramesToDeltas(frames);
+  const compString = compressFramesToStringGrids(frames);
+
+  const encDeltas = encodeFramesCompressed(compDeltas);
+  const encString = encodeFramesCompressed(compString);
+
+  const shortDeltas = encDeltas.map(addEncodings);
+  const shortString = encString.map(addEncodings);
+
+  const start = JSON.stringify(frames).length;
+  const d1 = JSON.stringify(compDeltas).length;
+  const d2 = JSON.stringify(encDeltas).length;
+  const d3 = JSON.stringify(shortDeltas).length;
+
+  const s1 = JSON.stringify(compString).length;
+  const s2 = JSON.stringify(encString).length;
+  const s3 = JSON.stringify(shortString).length;
+
+  const best = mergeCompressionResults(shortDeltas, shortString).join(';');
+  console.log(`uncompressed: ${start}`);
+
+  console.log('Deltas:');
+  console.log(`--compressed: ${stats(d2)}, ` + (100 * d1 / start).toFixed(2) + ' %');
+  console.log(`-----encoded: ${stats(d3)}, ` + (100 * d2 / d1).toFixed(2) + ' %');
+  console.log(`---shortened: ${stats(d3)}, ` + (100 * d3 / d2).toFixed(2) + ' %');
+  console.log('ratio: ' + (100 * d3 / start).toFixed(2) + ' %');
+  console.log('Grids:');
+  console.log(`--compressed: ${stats(s2)}, ` + (100 * s1 / start).toFixed(2) + ' %');
+  console.log(`-----encoded: ${stats(s3)}, ` + (100 * s2 / s1).toFixed(2) + ' %');
+  console.log(`---shortened: ${stats(s3)}, ` + (100 * s3 / s2).toFixed(2) + ' %');
+  console.log('ratio: ' + (100 * s3 / start).toFixed(2) + ' %');
+  console.log('BEST ratio: ' + (100 * best.length / start).toFixed(2) + ' %');
+  return best;
+}
+
+const saveToLocalStorage = (frames: Frame[]) => window.localStorage.setItem('animation', compressAndEncodeFrames(frames));
 const serializer = {
   saveToLocalStorage,
 }
